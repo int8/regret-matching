e569e76bac2c58250855a1c190854a255cb51dc9
kamil <kamil@czarnogorski.pl>
2018-09-25 08:34:46 +0000

    issue #2

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ t = 10000
 for i in range(0, t):
     a_move = a.move()
     b_move = b.move()
-    a.learn_from(a_move)
-    b.learn_from(b_move)
+    a.learn_from(b_move)
+    b.learn_from(a_move)
 
 _2e = np.round(2 * np.max([a.eps(), b.eps()]), 3)
 a_ne = a.current_best_response()
